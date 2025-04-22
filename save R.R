43b64c0fe32a249e126d8dbf4c8b8953f028ba94
Shadow0629') <sgunesekare@gmail.com')>
2025-04-22 14:24:29 +0530

    Initiat commit to netflix_titles project

--- save R.R
+++ save R.R
@@ -1,3 +1,29 @@
 library(readr)
 netflix_titles <- read_csv("netflix_titles.csv")
 View(netflix_titles)
+ 
+data(netflix_titles)
+
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+
+
+#  Count NA values per column using sapply()
+sapply(netflix_titles, function(x) sum(is.na(x)))
+
+#  Add column: title_length
+netflix_titles <- netflix_titles %>%
+  mutate(title_length = nchar(title))
+
+# Longest title
+netflix_titles %>%
+  filter(title_length == max(title_length, na.rm = TRUE)) %>%
+  select(title, title_length)
+
+# Average title length by type
+netflix_titles %>%
+  group_by(type) %>%
+  summarise(avg_title_length = mean(title_length, na.rm = TRUE))
+
+
